c6d7c095f3582d3cbc21d2837078eea9e0805965
gauthier-th <mail@gauthierth.fr>
2025-05-15 20:51:15 +0200

    feat: update the value of the MediaStatus.DELETED enum

--- server/lib/settings/migrations/0000_overseerr_merge.ts
+++ server/lib/settings/migrations/0000_overseerr_merge.ts
@@ -1,14 +1,29 @@
 import { MediaServerType } from '@server/constants/server';
 import type { AllSettings } from '@server/lib/settings';
 
-const overseerrMerge = (settings: any): AllSettings => {
+import { getRepository } from '@server/datasource';
+import Media from '@server/entity/Media';
+
+const overseerrMerge = async (settings: any): Promise<AllSettings> => {
   if (settings.main.mediaServerType) {
     return settings; // already migrated
   }
   const newSettings = { ...settings };
   newSettings.main.mediaServerType = MediaServerType.PLEX;
+
+  // New name
   newSettings.main.applicationTitle = 'Seerr';
   newSettings.notifications.agents.email.options.senderName = 'Seerr';
+
+  // MediaStatus.Blacklisted was added before MediaStatus. Deleted in Jellyseerr
+  const mediaRepository = getRepository(Media);
+  await mediaRepository
+    .createQueryBuilder()
+    .update(Media)
+    .set({ status: 7 })
+    .where('status = :status', { status: 6 })
+    .execute();
+
   return newSettings;
 };
 
